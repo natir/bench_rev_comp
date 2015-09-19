f4b16610abced6c306a201e50fd20572b189594a
Pierre Marijon <pierre@marijon.fr>
2015-09-19 20:51:58 +0200

    Add test for some GC %

--- src/main.cpp
+++ src/main.cpp
@@ -13,43 +13,47 @@
 #include "rev_comp_hash.hpp"
 #include "rev_comp_tab.hpp"
 #include "rev_comp_naive.hpp"
+#include "rev_comp_pol3.hpp"
+#include "rev_comp_pol2.hpp"
 
 int main(int argc, char** argv)
 {
-    std::string base_str = "CTAACCACCGTGTATTCATTATGGTACCAGGGACTTTAAGCCGAGTCAATGGAGCTCGCAATACAGAGTTTACCGCATCTTGCCGTAACTGACAAACTGT";
+    std::map<std::string, std::string> seq_list;
+    seq_list.emplace("100 nuc gc 10", "ATAAATATAATATAATTTTTTATTAAAATATTTTTATATATAATATAATTATATATTTTATAAAATTAATATATATATTAAATATTTTATAAATAATAAA");
+    seq_list.emplace("100 nuc gc 10", "TATATGATTTACAAAATATTTGTTATTTTTGAATATATACTTTATTTAATAGTTTTTTAGTATACAAATATTAATTATTATTTAAATTAGAATAATTTTA");
+    seq_list.emplace("100 nuc gc 20", "TATTTATAAAGTCCGCCATACAAATAATAATTCAAAAAAATTATTGATTAATTTATCTAAACGATAACCTTTTGTATCATATTAGAGCGGAAAGATAATT");
+    seq_list.emplace("100 nuc gc 30", "ATAATGCTGCTTATAATCTCTCTGTCGTTGTTAACGTTTATAGTCTAGTTTCATTATAATTGTATGTTATTGAGGCATTGATTAATACTGGAAAACATTT");
+    seq_list.emplace("100 nuc gc 40", "GAAATGAACTAATCTATACGACAGAAACCGTGTACCTACCAAATCTCTTTAATGTAAGTTCTGACTAATTCGTACTTTGTTAAGAACTTACATTTTAACA");
+    seq_list.emplace("100 nuc gc 50", "CTAACCACCGTGTATTCATTATGGTACCAGGGACTTTAAGCCGAGTCAATGGAGCTCGCAATACAGAGTTTACCGCATCTTGCCGTAACTGACAAACTGT");
+    seq_list.emplace("100 nuc gc 60", "ACAGGGGACATATGCCCTACCCCCATGATCTACTGGCGTCCCTGAGGCTGCAGTTCATGTGGTGGGGCAGCATCCGCGGCAAGCCCTAGTGCAATGGCGG");
+    seq_list.emplace("100 nuc gc 70", "TTTCCCGCCCCCGTTCCGAGGAAGAGGCGGCGCGGGTGCGGCCATCGCTAGTGCGGAAACTGGGGAGGCTCCCGGGCCTCCGCCTTTAGGCGGTGCCTGC");
+    seq_list.emplace("100 nuc gc 80", "CCCCTCGTAGAGGGGCTGCCAGCCATGCCCCGCGGGGGTCCGGGAAGGCGAGCCGGCCCGGCCGACCCGGGGGGGCGGGCCTCGGAGCCGCCCGGCGACG");
+    seq_list.emplace("100 nuc gc 90", "GCCGTGGGCCCGCGGCGGAACCCCCCCGGCGAGCCCCCGCGGGCGCCGGCTGGGCGGCCCCGGCCGGCCCGACCAGGGGCCCGGACGCGCCGCCCCACGG");
+    seq_list.emplace("100 nuc gc 100", "CCCCCGGGCCGGGGCGCGCCGCCCCGCGGGGGCCGGGGCCGCCCGCCGGCCCCCGCCGCGCCCGCGCCGCGCGCGCCCGCCCCCCCGCCCGCCCGCCCGG");
 
     std::map<std::string, std::unique_ptr<arev_comp> > bench_list;
 
     bench_list.emplace("tab", std::make_unique<rev_comp_tab>());
     bench_list.emplace("hash", std::make_unique<rev_comp_tab>());
     bench_list.emplace("naive", std::make_unique<rev_comp_naive>());
+    bench_list.emplace("pol3", std::make_unique<rev_comp_pol3>());
 
-    std::cout<<"Base sequence "<<base_str<<std::endl;
-    for(const auto &it : bench_list)
+    for(const auto &seq : seq_list)
     {
-	std::cout<<"Result for "<<it.first<<" "<<it.second->run(base_str)<<std::endl;
-    }
-    
-    for(const auto &it : bench_list)
-    {
-	auto begin = std::chrono::high_resolution_clock::now();
-        
-	for(auto i = 0; i != 10000; i++)
+	std::cout<<"Sequence with "<<seq.first<<std::endl;
+	for(const auto &algo : bench_list)
 	{
-	    it.second->run(base_str);
+	    auto begin = std::chrono::high_resolution_clock::now();
+        
+	    for(auto i = 0; i != 100000; i++)
+	    {
+		algo.second->run(seq.second);
+	    }
+
+	    auto elapsed = std::chrono::high_resolution_clock::now() - begin;
+
+	    long long duration = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+	    std::cout<<"\t100 000 call of "<<algo.first<<" algo : "<<duration<<" microseconds "<<std::endl;
 	}
-
-	auto elapsed = std::chrono::high_resolution_clock::now() - begin;
-
-	long long duration = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
-	std::cout<<"10 000 call of "<<it.first<<" algo : "<<duration<<" microseconds "<<std::endl;
     }
 }
-
-
-
-
-
-
-
-
