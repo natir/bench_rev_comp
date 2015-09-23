8b44747dbc7282d4b0ec37bba8a154d2c6653317
Pierre Marijon <pierre@marijon.fr>
2015-09-23 17:50:31 +0200

    cpp: exe use the new cli norm

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -17,48 +17,50 @@
 
 int main(int argc, char** argv)
 {
-    std::map<std::string, std::string> seq_list;
-    seq_list.emplace("0", "AAAAAAAATATTTAAAATTAAAAATAATTAATATTTTAAATTTTATTTAATTTATTTATATATTATTATAATAAATTAATAAAAATTTAATATTAAATAA");
-    seq_list.emplace("10", "TAAAATATTCTTAATTTTAATAAAAAATTAAAAGAAATATATATACAATATAAAGAAGTTAGAAAATTAATAAGAGAATTTTCTTTCTGTTTATTAAAGA");
-    seq_list.emplace("20", "TATTTATAAAGTCCGCCATACAAATAATAATTCAAAAAAATTATTGATTAATTTATCTAAACGATAACCTTTTGTATCATATTAGAGCGGAAAGATAATT");
-    seq_list.emplace("30", "ATAATGCTGCTTATAATCTCTCTGTCGTTGTTAACGTTTATAGTCTAGTTTCATTATAATTGTATGTTATTGAGGCATTGATTAATACTGGAAAACATTT");
-    seq_list.emplace("40", "GAAATGAACTAATCTATACGACAGAAACCGTGTACCTACCAAATCTCTTTAATGTAAGTTCTGACTAATTCGTACTTTGTTAAGAACTTACATTTTAACA");
-    seq_list.emplace("50", "CTAACCACCGTGTATTCATTATGGTACCAGGGACTTTAAGCCGAGTCAATGGAGCTCGCAATACAGAGTTTACCGCATCTTGCCGTAACTGACAAACTGT");
-    seq_list.emplace("60", "ACAGGGGACATATGCCCTACCCCCATGATCTACTGGCGTCCCTGAGGCTGCAGTTCATGTGGTGGGGCAGCATCCGCGGCAAGCCCTAGTGCAATGGCGG");
-    seq_list.emplace("70", "TTTCCCGCCCCCGTTCCGAGGAAGAGGCGGCGCGGGTGCGGCCATCGCTAGTGCGGAAACTGGGGAGGCTCCCGGGCCTCCGCCTTTAGGCGGTGCCTGC");
-    seq_list.emplace("80", "CCCCTCGTAGAGGGGCTGCCAGCCATGCCCCGCGGGGGTCCGGGAAGGCGAGCCGGCCCGGCCGACCCGGGGGGGCGGGCCTCGGAGCCGCCCGGCGACG");
-    seq_list.emplace("90", "GCCGTGGGCCCGCGGCGGAACCCCCCCGGCGAGCCCCCGCGGGCGCCGGCTGGGCGGCCCCGGCCGGCCCGACCAGGGGCCCGGACGCGCCGCCCCACGG");
-    seq_list.emplace("100", "CCCCCGGGCCGGGGCGCGCCGCCCCGCGGGGGCCGGGGCCGCCCGCCGGCCCCCGCCGCGCCCGCGCCGCGCGCGCCCGCCCCCCCGCCCGCCCGCCCGG");
+    /* Take argument */
+    long long repeat = std::stoll(std::string(argv[1]));
+    std::string seq(argv[2]);
 
+    /* Initialize algorithme */
     std::map<std::string, std::unique_ptr<arev_comp> > bench_list;
-
-    // bench_list.emplace("tab", std::make_unique<rev_comp_tab>());
-    // bench_list.emplace("hash", std::make_unique<rev_comp_tab>());
-    // bench_list.emplace("naive", std::make_unique<rev_comp_naive>());
+    bench_list.emplace("tab", std::make_unique<rev_comp_tab>());
+    bench_list.emplace("hash", std::make_unique<rev_comp_tab>());
+    bench_list.emplace("naive", std::make_unique<rev_comp_naive>());
     bench_list.emplace("pol3", std::make_unique<rev_comp_pol3>());
 
-    std::cout<<"gc%";
+    /* print algorithme name */
+    bool first = true;
     for(const auto &pair : bench_list)
-	std::cout<<","<<pair.first;
-    std::cout<<std::endl;
-    
-    for(const auto &seq : seq_list)
     {
-	std::cout<<seq.first;
-	for(const auto &algo : bench_list)
-	{
-	    auto begin = std::chrono::high_resolution_clock::now();
-        
-	    for(auto i = 0; i != 1000000; i++)
-	    {
-		algo.second->run(seq.second);
-	    }
-
-	    auto elapsed = std::chrono::high_resolution_clock::now() - begin;
-
-	    long long duration = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
-	    std::cout<<","<<duration;
-	}
-	std::cout<<std::endl;
+	if(first)
+	    std::cout<<pair.first;
+	else
+	    std::cout<<","<<pair.first;
+	first = false;
     }
+    std::cout<<std::endl;
+
+    /* loop on all algorithme and print result */
+    first = true;
+    for(const auto &algo : bench_list)
+    {
+	auto begin = std::chrono::high_resolution_clock::now();
+        
+	for(auto i = 0; i != repeat; i++)
+	{
+	    algo.second->run(seq);
+	}
+
+	auto elapsed = std::chrono::high_resolution_clock::now() - begin;
+
+	long long duration = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+	if(first)
+	    std::cout<<duration;
+	else
+	    std::cout<<","<<duration;
+	first = false;
+    }
+    std::cout<<std::endl;
+
+    return 0;
 }
