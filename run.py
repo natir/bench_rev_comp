259bc5c167f233771a60bffc7cf6405773a2f2c7
Pierre Marijon <pierre@marijon.fr>
2015-09-30 10:43:37 +0200

    Add some progress information

--- run.py
+++ run.py
@@ -63,7 +63,13 @@ def read_run_store(lang, nb_call, output, seq_file, nb_of_nuc):
     all_data = defaultdict(list)
     with open(seq_file) as csvfile:
         seq_reader = csv.DictReader(csvfile)
+
+        print("Run")
+
         for row in seq_reader:
+
+            print("    len : "+row["length"]+" gc : "+row["gc"])
+
             tmp_dict = run(lang+"/bench", row["seq"], row["gc"], nb_call,
                             nb_of_nuc/len(row["seq"]))
             for k in tmp_dict.keys():
@@ -82,11 +88,14 @@ def run(bin_path, seq, gc, nb_call, nb_repeat):
         result_reader = csv.DictReader(result.stdout)
         for row in result_reader:
             for algo in row.keys():
-                ret[str(len(seq))+"_"+str(gc)+"_"+algo].append(int(row[algo]))
+                ret[str(len(seq))+"-"+str(gc)+"-"+algo].append(int(row[algo]))
 
     return ret
 
 def store(result, output, lang, nb_call):
+
+    print("Store")
+    
     with open(output+"_"+lang+"_raw.csv", "w") as raw, open(
             output+"_resume.csv", "w") as clean:
         raw.write(",".join(["len", "gc", "algo"] + ["res_"+str(i) for i in
@@ -94,7 +103,7 @@ def store(result, output, lang, nb_call):
         clean.write(",".join(["language", "len", "gc", "algo", "mean",
                               "stderror"]) + "\n")
         for len_gc_algo in result:
-            (length, gc, algo) = len_gc_algo.split("_")
+            (length, gc, algo) = len_gc_algo.split("-")
             raw.write(",".join([length, gc, algo] +
                                [str(i) for i in result[len_gc_algo]]) + "\n")
 
@@ -104,6 +113,9 @@ def store(result, output, lang, nb_call):
                                  + "\n")
 
 def generate_graph(output):
+
+    print("Generate graphe")
+    
     pandas.set_option('display.mpl_style', 'default')
     data = pandas.read_csv(output+"_resume.csv")
 
@@ -123,9 +135,7 @@ def generate_graph(output):
                                                       is_algo]['stderror'])))
                 df[algo] = mean
 
-            print(df)
             df.sort_index(inplace=True)
-            print(df)
             graph = df.plot(kind='bar', yerr=errors)
 
             fig = graph.get_figure()
